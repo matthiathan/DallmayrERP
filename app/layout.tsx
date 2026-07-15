0a0a45a1b6e0c54d7160d757c208464093f36447
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-15 11:53:24 +0200

    Import 4K ultrawide layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './hamster-loader.css';
 import './feature-widgets.css';
 import './navigation.css';
 import './spatial-desktop.css';
+import './ultrawide.css';
 import './mobile.css';
 import { AuthProvider } from '@/components/auth/AuthProvider';
 
