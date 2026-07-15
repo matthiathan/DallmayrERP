f7c3ab02b25328b2a95f0f4dd99b00fb74c153e7
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-15 10:15:17 +0200

    Add executive command centre page

---
+++ app/executive/command-centre/page.tsx
@@ -0,0 +1,10 @@
+import { EnterpriseCommandCentre } from '@/components/features/EnterpriseCommandCentre';
+import { AppShell } from '@/components/layout/AppShell';
+
+export default function ExecutiveCommandCentrePage() {
+  return (
+    <AppShell>
+      <EnterpriseCommandCentre />
+    </AppShell>
+  );
+}
