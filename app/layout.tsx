370db63a52a4321f10aa2bac702fad6cc38d9a1c
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-29 10:51:26 +0200

    Add mobile master-detail workflows and sticky actions (#35)
    * Add mobile master-detail workflow controller
    
    * Add mobile master-detail and sticky action styles
    
    * Mount mobile workflow enhancer

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,11 +53,13 @@ import './mobile-navigation-drawer.css';
 import './mobile-data-views.css';
 import './mobile-application-layout.css';
 import './mobile-application-layout-final.css';
+import './mobile-master-detail-actions.css';
 import { AccessStatusGuard } from '@/components/auth/AccessStatusGuard';
 import { AuthProvider } from '@/components/auth/AuthProvider';
 import { AppearanceProvider } from '@/components/appearance/AppearanceProvider';
 import { RenderedSurfaceContrastSync } from '@/components/appearance/RenderedSurfaceContrastSync';
 import { GlobalAccountMenu } from '@/components/layout/GlobalAccountMenu';
+import { MobileWorkflowEnhancer } from '@/components/ui/MobileWorkflowEnhancer';
 
 const APPEARANCE_BOOT_SCRIPT = `
 (function () {
@@ -169,6 +171,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <AppearanceProvider>
             <RenderedSurfaceContrastSync />
             <AccessStatusGuard>
+              <MobileWorkflowEnhancer />
               <GlobalAccountMenu />
               {children}
             </AccessStatusGuard>
