8fb1449d620159f8234f10299baee3b3ef3c8c92
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-29 10:59:40 +0200

    Add stock scanner centre route

---
+++ app/warehouse/stock/scan/page.tsx
@@ -0,0 +1,10 @@
+import { MobileScannerCentre } from '@/components/features/MobileScannerCentre';
+import { AppShell } from '@/components/layout/AppShell';
+
+export default function StockScannerPage() {
+  return (
+    <AppShell>
+      <MobileScannerCentre />
+    </AppShell>
+  );
+}
