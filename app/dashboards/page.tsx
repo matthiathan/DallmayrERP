a6facabf3215da38bcdd33db56c992257655f85b
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-30 15:27:49 +0200

    Add shared dashboards route

---
+++ app/dashboards/page.tsx
@@ -0,0 +1,10 @@
+import { SharedDashboardWorkspace } from '@/components/features/SharedDashboardWorkspace';
+import { AppShell } from '@/components/layout/AppShell';
+
+export default function SharedDashboardsPage() {
+  return (
+    <AppShell>
+      <SharedDashboardWorkspace />
+    </AppShell>
+  );
+}
