2d6cc6f13452d6be7a6464c5ac19b95835f1178f
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-08-05 08:17:57 +0200

    Route messaging through role-authorised work namespace

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,13 +1,6 @@
-import { notFound } from 'next/navigation';
-import { AppShell } from '@/components/layout/AppShell';
-import { InternalMessagingWorkspace } from '@/components/features/InternalMessagingWorkspace';
+import { notFound, redirect } from 'next/navigation';
 
 export default function MessagesPage() {
   if (process.env.NEXT_PUBLIC_INTERNAL_MESSAGING_ENABLED === 'false') notFound();
-
-  return (
-    <AppShell>
-      <InternalMessagingWorkspace />
-    </AppShell>
-  );
+  redirect('/work/messages');
 }
