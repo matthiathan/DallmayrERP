a8e051f8060181c062e5d22583efb3db7e3d628c
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-08-13 11:53:05 +0200

    Fail closed on messaging route flag

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,6 +1,6 @@
 import { notFound, redirect } from 'next/navigation';
 
 export default function MessagesPage() {
-  if (process.env.NEXT_PUBLIC_INTERNAL_MESSAGING_ENABLED === 'false') notFound();
+  if (process.env.NEXT_PUBLIC_INTERNAL_MESSAGING_ENABLED !== 'true') notFound();
   redirect('/work/messages');
 }
