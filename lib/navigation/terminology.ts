41f1abfbb8837ccf30ae165629e228c0b7eae917
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-08-14 13:22:50 +0200

    Add canonical navigation terminology

---
+++ lib/navigation/terminology.ts
@@ -0,0 +1,5 @@
+export const TODAY_LABEL = 'Today';
+export const MY_WORK_LABEL = 'My Work';
+
+export const TODAY_OPEN_LABEL = `Open ${TODAY_LABEL}`;
+export const TODAY_NAVIGATION_LABEL = `${TODAY_LABEL} navigation`;
