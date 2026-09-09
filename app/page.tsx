96db9382f53b3617d2b6f4abd17d7570c55b5197
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-09-09 08:28:34 +0200

    feat: add data usage to fleet overview

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { FleetDataUsagePanel } from '@/components/features/FleetDataUsagePanel';
 import { FleetVisualCommandCenter } from '@/components/features/FleetVisualCommandCenter';
 import { AppShell } from '@/components/layout/AppShell';
 
@@ -5,6 +6,7 @@ export default function DashboardPage() {
   return (
     <AppShell>
       <FleetVisualCommandCenter />
+      <FleetDataUsagePanel />
     </AppShell>
   );
 }
