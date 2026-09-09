e96236aa11a66dccc33cf1058ed963cd36809eb1
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-09-09 10:32:04 +0200

    refactor: remove obsolete machines wrapper

--- components/features/MachinesManagementPage.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { MachineFleetBrowser } from '@/components/telemetry-platform/MachineFleetBrowser';
-
-export function MachinesManagementPage() {
-  return <MachineFleetBrowser />;
-}
