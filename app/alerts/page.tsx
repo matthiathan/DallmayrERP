73d9b157b40403abd8bb4ebe687205da863fabfb
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-09-09 09:39:14 +0200

    feat: switch alerts to ground-up alarm center

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -1,14 +1,10 @@
-import { FleetAlertPulse } from '@/components/features/FleetAlertPulse';
-import { MachineTelemetryOverview } from '@/components/features/MachineTelemetryOverview';
 import { AppShell } from '@/components/layout/AppShell';
+import { AlarmCenter } from '@/components/telemetry-platform/AlarmCenter';
 
 export default function AlertsPage() {
   return (
     <AppShell>
-      <div className="alerts-visual-route">
-        <FleetAlertPulse />
-        <MachineTelemetryOverview initialStatus="fault" />
-      </div>
+      <AlarmCenter />
     </AppShell>
   );
 }
