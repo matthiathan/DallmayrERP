e335d4c410ca54b78abe393e896e300ad5c81dee
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-09-16 09:10:12 +0200

    Add fault catalogue workbench to alerts

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -1,11 +1,13 @@
 import { AppShell } from '@/components/layout/AppShell';
 import { AlarmCenter } from '@/components/telemetry-platform/AlarmCenter';
 import { DeviceAttentionPanel } from '@/components/telemetry-platform/DeviceAttentionPanel';
+import { FaultIntelligencePanel } from '@/components/telemetry-platform/FaultIntelligencePanel';
 
 export default function AlertsPage() {
   return (
     <AppShell>
       <DeviceAttentionPanel />
+      <FaultIntelligencePanel management />
       <AlarmCenter />
     </AppShell>
   );
