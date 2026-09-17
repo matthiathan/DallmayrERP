885fc690ba904a57f55a88dcb24f640793850cc1
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-09-17 08:48:53 +0200

    Use official Dallmayr South Africa brand assets

---
+++ lib/brand/dallmayr.ts
@@ -0,0 +1,8 @@
+export const DALLMAYR_SA_SITE = 'https://dallmayr.co.za';
+
+// Official public brand assets used by dallmayr.co.za.
+export const DALLMAYR_SA_LOGO_URL =
+  'https://dallmayr.co.za/wp-content/uploads/2022/10/cropped-dallmayr-logo-transparent-1-105x70.png';
+
+export const DALLMAYR_SA_ICON_URL =
+  'https://dallmayr.co.za/wp-content/uploads/2022/10/cropped-dallmayr-logo-dark-120x104.png';
